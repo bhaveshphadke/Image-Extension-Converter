9922bb906ac85b1c3734ec9f6d8fe8313539ee8c
Bhavesh Phadke <bhaveshphadke5256@gmail.com>
2022-05-20 13:12:51 +0530

    commit

--- static/js/login.js
+++ static/js/login.js
@@ -3,6 +3,7 @@ checkLogin=async()=>{
     // e.preventDefault();
     let email = await document.getElementById('email').value;
     let password = await document.getElementById('password').value;
+    console.log('1');
     const response = await fetch('/auth/login',{
         method:'POST',
         headers:{
@@ -10,10 +11,15 @@ checkLogin=async()=>{
         },
         body:JSON.stringify({email:email,password:password})
     })
+    console.log('2');
+
     const json = await response.json()
     console.log(json.isLoggedIn);
+    console.log('3');
 
    if(json.isLoggedIn){
+    console.log('4');
+
     localStorage.setItem('login',true)
     window.location = '/'
    }
